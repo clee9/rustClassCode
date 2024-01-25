091d4d94557bdd8123111d50763b3d71396c1ef7
unknown <tjarrett@trinity.edu>
2024-01-24 20:07:00 -0600

    hash module functioning

--- ch78/src/main.rs
+++ ch78/src/main.rs
@@ -18,14 +18,25 @@ pub mod collections {
     }
 
     pub mod hash {
+        use std::collections::HashMap;
 
+        pub fn init_hash() -> HashMap<String, u32> {
+            HashMap::new()
+        }
+        
+        pub fn get_hash<'a, 'b>(hash: &'a HashMap<String, u32>, word: &'b String) -> Option<&'a u32> {
+            hash.get(word)
+        }
+
+        pub fn add_to_hash(hash: & mut HashMap<String, u32>, input: &String, count: u32) {
+            hash.insert(input.clone(), count);
+        }
     }
 }
 
 fn main() {
     let mut v = collections::vector::init_vec();
-
-    
+    let mut hash = collections::hash::init_hash();
     loop {
         let mut word = collections::string::init_string();
         println!("Enter a word. To exit, type 'exit'");
@@ -34,8 +45,21 @@ fn main() {
         match &word[..] {
             "exit" => break,
             "output" => todo!("print vec as string"),
-            "hash" => todo!("hash"),
-            _ => v.push(word),
+            "hash" => {
+                for(key, value) in &hash {
+                    println!("{key}: {value}");
+                }
+            },
+            _ => {
+                let value: Option<&u32> = collections::hash::get_hash(&hash, &word);
+                let count;
+                match value {
+                    Some(v) => count = v + 1,
+                    None => count = 1
+                }
+                collections::hash::add_to_hash(&mut hash, &word, count);
+                v.push(word);
+            },
         }
     }
 }
